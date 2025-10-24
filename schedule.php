7418d28e7b9fe5d21923277adc3ccceeeed2e706
devwithcha <chacator.dev@gmail.com>
2025-10-25 02:32:45 +0800

    Cors helper modified

--- schedule.php
+++ schedule.php
@@ -1,13 +1,5 @@
 <?php
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
+include 'cors_helper.php';
 
 require_once __DIR__ . '/connect.php';
 @include_once __DIR__ . '/activity_logger.php';
