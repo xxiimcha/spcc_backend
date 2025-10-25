917316aa61563f7b0b98a1163f5269488e67b282
devwithcha <chacator.dev@gmail.com>
2025-10-25 13:06:56 +0800

    Cors helper used in Auth php

--- auth.php
+++ auth.php
@@ -1,15 +1,5 @@
-<?php
-declare(strict_types=1);
-
-header('Content-Type: application/json; charset=utf-8');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-
-if (($_SERVER['REQUEST_METHOD'] ?? '') === 'OPTIONS') {
-  http_response_code(204);
-  exit();
-}
+<?phpss
+include 'cors_helper.php';
 
 require_once __DIR__ . '/connect.php';
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
